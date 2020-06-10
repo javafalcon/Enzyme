6a9ccb37624c06d781f54db572a1a1f9950a6cd4
javafalcon <javafalcon@163.com>
2020-06-10 13:46:07 +0800

    6-10-20

--- prepareDataset.py
+++ prepareDataset.py
@@ -7,8 +7,10 @@ Created on Mon Jun  8 13:12:44 2020
 import os
 from Bio import SeqIO
 import numpy as np
+from SeqFormulate import DAA_chaosGraph
+from sklearn.model_selection import StratifiedKFold
 
-def readAllSeqs():
+def readAllSeqsML():
     files=['ec_1.fasta', 'ec_2.fasta', 'ec_3.fasta', 'ec_4.fasta', 'ec_5.fasta',
            'ec_6.fasta', 'ec_7.fasta']
     data={}
@@ -25,37 +27,67 @@ def readAllSeqs():
             if protId in data.keys():
                 target[protId] = t
             else:
-                data[protId] = seq_record.seq
+                data[protId] = str(seq_record.seq)
                 target[protId] = t
     
     return data, target
 
-def readNr40(data, target):
-    '''files=['ec_1_40.fasta', 'ec_2_40.fasta', 'ec_3_40.fasta', 'ec_4_40.fasta',
-           'ec_5_40.fasta', 'ec_6_40.fasta', 'ec_7_40.fasta']'''
-    files=['ec_1.fasta', 'ec_2.fasta', 'ec_3.fasta', 'ec_4.fasta', 'ec_5.fasta',
-           'ec_6.fasta', 'ec_7.fasta']
-    nrsequens={}
-    nrtargets={}
-    targ = []
-    fw = open('multiLabelEnzymes_All.txt', 'a')
+# read nr40 data as single label
+# Proteins who have multi lables were removed
+def readNr40SL():
+    files=['ec_1_40.fasta', 'ec_2_40.fasta', 'ec_3_40.fasta', 'ec_4_40.fasta',
+           'ec_5_40.fasta', 'ec_6_40.fasta', 'ec_7_40.fasta']
+    '''files=['ec_1.fasta', 'ec_2.fasta', 'ec_3.fasta', 'ec_4.fasta', 'ec_5.fasta',
+           'ec_6.fasta', 'ec_7.fasta']'''
+    prot_seqs = {}
+    prot_labels = {}
+    
     for i in range(7):
         file = os.path.join('data', files[i])
         for seq_record in SeqIO.parse(file, 'fasta'):
             seqid = seq_record.id
             seqid = seqid.split('|')
             seqid = seqid[1]
-            if seqid in nrsequens.keys():
-                #fw.write('{} has multi-label\n'.format(seqid))
-                targ.append(target[seqid])
+            if seqid in prot_seqs.keys():
+                # The protein has multi-lables, so remove it from protein dict
+                prot_seqs.pop(seqid)
+                prot_labels.pop(seqid)
             else:
-                nrsequens[seqid] = seq_record.seq
-                nrtargets[seqid] = target[seqid]
-    fw.close()
-    #return nrsequens, nrtargets
-    return targ
+                # The protien is firstly seen, so add it to protein dict
+                prot_seqs[seqid] = str(seq_record.seq)
+                prot_labels[seqid] = i
     
+    return prot_seqs, prot_labels
+
+
+def load_data():
+    prot_seqs, prot_labels = readNr40SL()
+    seqs, labels = [], []
+    for key in prot_seqs.keys():
+        seqs.append(prot_seqs[key])
+        labels.append(prot_labels[key])
+    X = DAA_chaosGraph(seqs)
+    y = np.array(labels)
+    return X, y
+
+def load_Kf_data(kfold=5, random_state=None):
+    X_train_Kf, y_train_Kf = [], []
+    X_test_Kf, y_test_Kf = [], []
+    X, y = load_data()
+    skf = StratifiedKFold(n_splits=kfold, random_state=random_state, shuffle=True)
+    for train_index, test_index in skf.split(X, y):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        
+        X_train_Kf.append(X_train)
+        y_train_Kf.append(y_train)
+        
+        X_test_Kf.append(X_test)
+        y_test_Kf.append(y_test)
+        
+    return (X_train_Kf, y_train_Kf), (X_test_Kf, y_test_Kf)
+
 if __name__ == "__main__":   
-    nrsequens, nrtargets = readNr40(data, target)        
+    X, y = load_data()
         
         
